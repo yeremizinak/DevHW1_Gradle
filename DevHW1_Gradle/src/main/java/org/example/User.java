1793b6c4234e9c904e56c784823af1cf6a14f6a2
yeremizinak <eremizinaekaterina@gmail.com>
2023-09-17 19:38:50 +0300

    init

---
+++ DevHW1_Gradle/src/main/java/org/example/User.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class User {
+    public String name, lastName;
+
+    public User(String name, String lastName){
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
